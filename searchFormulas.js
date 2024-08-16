f7dd87fbab0dc026e7fffbc6325a0e62f108b05d
KRadusz <kraduszewski@bedec.co.uk>
2024-08-16 13:18:13 +0100

    made changes to the reading ability as well as display issues of it. Now the search should be fixed

--- searchFormulas.js
+++ searchFormulas.js
@@ -87,6 +87,7 @@ class FindColours {
   constructor(data) {
     this.data = data;
   }
+
   searchValues() {
     var limitNum = 50;
     var loopNum = 0;
@@ -96,38 +97,45 @@ class FindColours {
     var card_name = document.getElementById("card_name");
     var colour_name = document.getElementById("colour_name");
     var pack_size = document.getElementById("pack_size");
-    for (var i = 0; i < this.data.length; i++) {
-      // skip if info is null
-      if (!this.data[i].CARDNAME) {
-        continue;
-      }
 
+    for (var i = 0; i < this.data.length; i++) {
       // check Product name input
-      // console.log(this.data[i]);
       if (product_name.value !== "any" && this.data[i].PRODUCTNAME.toLowerCase() !== colourCodeArr[product_name.value].toLowerCase()) {
         continue;
       }
 
-      // check colour card name name input
+      // check colour card name input
       if (card_name.value !== "any" && this.data[i].CARDNAME.toLowerCase() !== cardNameArr[card_name.value].toLowerCase()) {
         continue;
       }
 
       // check can size
-
-      if (this.data[i].PACKSIZE == null || this.data[i].PACKSIZE.toLowerCase() !== pack_size.value.toLowerCase()) {
+      if (this.data[i].PACKSIZE.toLowerCase() !== pack_size.value.toLowerCase()) {
         continue;
       }
-
+      // if (this.data[i].CARDNAME == "RAL 840HR") {
+      //   console.log(this.data[i]);
+      //   break;
+      // }
       // check colour name input
-      // set colour name to lower
-      var colourName = this.data[i].COLOURNAME.toLowerCase();
-      var colourCode = this.data[i].COLOURCODE.toLowerCase();
-      var colourAltColour = this.data[i].ALTCOLOURCODE.toLowerCase();
+      if (typeof this.data[i].COLOURNAME === "string" && this.data[i].COLOURNAME.trim() !== "") {
+        var colourName = this.data[i].COLOURNAME.toLowerCase();
+      }
+
+      if (typeof this.data[i].COLOURCODE === "string" && this.data[i].COLOURCODE.trim() !== "") {
+        var colourCode = this.data[i].COLOURCODE.toLowerCase();
+      }
+
+      if (typeof this.data[i].ALTCOLOURCODE === "string" && this.data[i].ALTCOLOURCODE.trim() !== "") {
+        var colourAltColour = this.data[i].ALTCOLOURCODE.toLowerCase();
+      }
 
       // check text in colour name if needed
-      if (colourName.includes("to complement*?")) {
-        this.data[i].COLOURNAME = this.data[i].COLOURNAME.split("To Complement*?")[1];
+      if (colourName && colourName.includes("to complement*?")) {
+        const splitParts = this.data[i].COLOURNAME.split("To Complement*?");
+        if (splitParts.length > 1) {
+          this.data[i].COLOURNAME = splitParts[1];
+        }
       }
 
       if (colourCode.includes(colour_name.value.toLowerCase()) || colourName.includes(colour_name.value.toLowerCase()) || colourAltColour.includes(colour_name.value.toLowerCase())) {
@@ -139,98 +147,45 @@ class FindColours {
         }
       }
     }
+
     return found_results;
   }
 }
 
 async function SearchCodes() {
-  // Call the function
-
   const BarCodeData = await fetchAndConcatenate(urls);
-  var ColourVal = new FindColours(BarCodeData);
-  var found_results = ColourVal.searchValues();
-  var limitNum = 50;
-  // put into body
+  const found_results = new FindColours(BarCodeData).searchValues();
+  const limitNum = 50;
+
   const result_body = document.getElementById("result_body");
   const no_results = document.getElementById("no_results");
   const result_table = document.getElementById("result_table");
   const too_many_results = document.getElementById("too_many_results");
-  // if length is 0
-  if (found_results.length > 0) {
-    result_table.style.display = "";
-    no_results.style.display = "none";
 
-    if (found_results.length == limitNum) {
-      too_many_results.style.display = "";
-    } else {
-      too_many_results.style.display = "none";
-    }
-  } else {
-    no_results.style.display = "";
-    result_table.style.display = "none";
-    too_many_results.style.display = "none";
-  }
-  result_body.innerHTML = "";
-  for (i = 0; i < found_results.length; i++) {
-    const tr = document.createElement("tr");
-    const pd = document.createElement("td");
-    pd.innerHTML = found_results[i].PRODUCTNAME;
-    const cc = document.createElement("td");
-    cc.innerHTML = found_results[i].COLOURCODE;
-    const ccn = document.createElement("td");
-    // if it includes long txt
-    if (found_results[i].CARDNAME.includes("Bedec Complementary Colours")) {
-      var split_info = found_results[i].CARDNAME;
-      ccn.innerHTML = split_info.split("Bedec Complementary Colours - ")[1];
-    } else {
-      ccn.innerHTML = found_results[i].CARDNAME;
-    }
+  // Toggle visibility of results, too many results, and no results messages
+  result_table.style.display = found_results.length > 0 ? "" : "none";
+  no_results.style.display = found_results.length > 0 ? "none" : "";
+  too_many_results.style.display = found_results.length === limitNum ? "" : "none";
 
-    const cn = document.createElement("td");
-    cn.innerHTML = found_results[i].COLOURNAME;
-    const bc = document.createElement("td");
-    bc.innerHTML = found_results[i].BASECODE;
-    const pack_size = document.createElement("td");
-    pack_size.innerHTML = found_results[i].PACKSIZE;
-    const rgb_td = document.createElement("td");
-    rgb_td.style.backgroundColor = found_results[i].RGB_HTML;
-    const select_td = document.createElement("td");
-    const select_btn = document.createElement("button");
-    select_btn.className = "btn btn-sm btn-primary m-2";
-    select_btn.innerHTML = "Select";
-    select_btn.setAttribute(
-      "onclick",
-      "ViewBarcode('" +
-        pd.innerHTML +
-        "','" +
-        bc.innerHTML +
-        "','" +
-        found_results[i].BrewersProductCode +
-        "','" +
-        pack_size.innerHTML +
-        "','" +
-        Number(found_results[i].BedecProductBarcode) +
-        "','" +
-        found_results[i].COLOURNAME +
-        "','" +
-        found_results[i].BrewersProductCode +
-        "')"
-    );
+  result_body.innerHTML = found_results
+    .map((result) => {
+      const cardName = result.CARDNAME.includes("Bedec Complementary Colours") ? result.CARDNAME.split("Bedec Complementary Colours - ")[1] : result.CARDNAME;
 
-    select_td.appendChild(select_btn);
-
-    found_results[i].COLOURCODE;
-    tr.appendChild(pd);
-    tr.appendChild(cc);
-    tr.appendChild(ccn);
-    tr.appendChild(cn);
-    tr.appendChild(bc);
-    tr.appendChild(pack_size);
-    tr.appendChild(rgb_td);
-    tr.appendChild(select_td);
-
-    result_body.appendChild(tr);
-  }
+      return `
+      <tr>
+        <td>${result.PRODUCTNAME}</td>
+        <td>${result.COLOURCODE}</td>
+        <td>${cardName}</td>
+        <td>${result.COLOURNAME}</td>
+        <td>${result.BASECODE}</td>
+        <td>${result.PACKSIZE}</td>
+        <td style="background-color:${result.RGB_HTML};"></td>
+        <td><button class="btn btn-sm btn-primary m-2" onclick="ViewBarcode('${result.PRODUCTNAME}', '${result.BASECODE}', '${result.BrewersProductCode}', '${result.PACKSIZE}', '${Number(
+        result.BedecProductBarcode
+      )}', '${result.COLOURNAME}', '${result.BrewersProductCode}')">Select</button></td>
+      </tr>`;
+    })
+    .join("");
 }
 
 function ViewBarcode(product_name, base_name, product_code, packsize, barcode, colour_name, rgb_colour) {
